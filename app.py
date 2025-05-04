56b6661d4fde28b408f4c5025c6ebbfa0ba20335
Nic11371 <nic11371@gmail.com>
2025-05-04 22:58:40 +0300

    webhook

--- app.py
+++ app.py
@@ -1,35 +1,92 @@
-import asyncio
 import os
-import logging
-from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from dotenv import load_dotenv
 from handlers.user import user
 from handlers.creator import creator
 from handlers.admin import admin
-from database.models import async_main
-from middlewares import AntiSpamMiddleware
+import logging
+from aiogram.types import BotCommand, BotCommandScopeDefault
+from aiohttp import web
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+
 
 load_dotenv()
 
 
-async def main():
-    bot = Bot(token=os.getenv("TOKEN"))
-    dp = Dispatcher()
-    dp.include_routers(user, creator, admin)
-    dp.update.middleware(AntiSpamMiddleware(limit=2, interval=1))
-    dp.startup.register(startup)
+# переменные для работы
+ADMIN_ID = os.getenv('ADMIN_ID')
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+HOST = os.getenv("HOST")
+PORT = int(os.getenv("PORT"))
+WEBHOOK_PATH = f'/{BOT_TOKEN}'
+BASE_URL = os.getenv("BASE_URL")
+
+# инициализируем бота и диспетчера для работы с ним
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+
+
+async def set_commands():
+    # Создаем список команд, которые будут доступны пользователям
+    commands = [BotCommand(command='start', description='Старт')]
+    # Устанавливаем эти команды как дефолтные для всех пользователей
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
+
+
+# Функция, которая будет вызвана при запуске бота
+async def on_startup() -> None:
+    # Устанавливаем командное меню
+    await set_commands()
+    # Устанавливаем вебхук для приема сообщений через заданный URL
+    await bot.set_webhook(f"{BASE_URL}{WEBHOOK_PATH}")
+    # Отправляем сообщение администратору о том, что бот был запущен
+    await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен!')
+
+
+# Функция, которая будет вызвана при остановке бота
+async def on_shutdown() -> None:
+    # Отправляем сообщение администратору о том, что бот был остановлен
+    await bot.send_message(chat_id=ADMIN_ID, text='Бот остановлен!')
+    # Удаляем вебхук и, при необходимости, очищаем ожидающие обновления
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    # Закрываем сессию бота, освобождая ресурсы
+    await bot.session.close()
 
 
-async def startup(dispatcher: Dispatcher):
-    await async_main()
+# Основная функция, которая запускает приложение
+def main() -> None:
+    # Подключаем маршрутизатор (роутер) для обработки сообщений
+    dp.include_routers(user, creator, admin)
+
+    # Регистрируем функцию, которая будет вызвана при старте бота
+    dp.startup.register(on_startup)
+
+    # Регистрируем функцию, которая будет вызвана при остановке бота
+    dp.shutdown.register(on_shutdown)
+
+    # Создаем веб-приложение на базе aiohttp
+    app = web.Application()
+
+    # Настраиваем обработчик запросов для работы с вебхуком
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,  # Передаем диспетчер
+        bot=bot  # Передаем объект бота
+    )
+    # Регистрируем обработчик запросов на определенном пути
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+
+    # Настраиваем приложение и связываем его с диспетчером и ботом
+    setup_application(app, dp, bot=bot)
+
+    # Запускаем веб-сервер на указанном хосте и порте
+    web.run_app(app, host=HOST, port=PORT)
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    print("Бот включен!")
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("Бот выключен!")
+# Точка входа в программу
+if __name__ == "__main__":
+    # Настраиваем логирование (информация, предупреждения, ошибки) и выводим их в консоль
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)  # Создаем логгер для использования в других частях программы
+    main()  # Запускаем основную функцию
